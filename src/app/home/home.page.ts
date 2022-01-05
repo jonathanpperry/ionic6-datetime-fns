5edbd35445dafb2177e0c15d0c64981b962e2407
Jonathan Perry <jpper@umich.edu>
2022-01-05 11:57:08 -0500

    Implemented the Ionic datetime presentation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-hoame',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  modes = [
+    'date',
+    'date-time',
+    'month',
+    'month-year',
+    'time',
+    'time-date',
+    'year',
+  ];
+
+  selectedMode = 'date';
+  showPicker = false;
 
   constructor() {}
-
 }
