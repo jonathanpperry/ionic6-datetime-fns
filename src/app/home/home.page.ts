c8dbb75200b97b93b2c122637aebe468209355d7
Jonathan Perry <jpper@umich.edu>
2022-01-18 10:31:19 -0500

    Added android platform and implemented ionic datetime parsing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { format, parseISO } from 'date-fns';
 
 @Component({
   selector: 'app-hoame',
@@ -19,5 +20,23 @@ export class HomePage {
   selectedMode = 'date';
   showPicker = false;
 
-  constructor() {}
+  dateValue = format(new Date(), 'yyyy-MM-dd') + 'T09:00:00.000Z';
+  formattedString = '';
+
+  constructor() {
+    this.setToday();
+  }
+
+  setToday() {
+    this.formattedString = format(
+      parseISO(format(new Date(), 'yyyy-MM-dd') + 'T09:00:00.000Z'),
+      'HH:mm, MMM d, yyyy'
+    );
+  }
+
+  dateChanged(value) {
+    this.dateValue = value;
+    this.formattedString = format(parseISO(value), 'HH:mm, MMM dd yyyy');
+    this.showPicker = false
+  }
 }
